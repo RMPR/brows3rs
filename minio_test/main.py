dbd1a1be8ec2d497bc00852791854a929a46ce5a
Vaibhav <vaibhav.awale@zivid.com>
2024-04-02 14:56:04 +0200

    Update python example

--- minio_test/main.py
+++ minio_test/main.py
@@ -1,9 +1,11 @@
-import minio
+from minio import Minio, Object, error
 import os
 
+# Use minio==6 for this example
+
 def _main():
-    bucket_name = "se-ci-artifacts"
-    minio = minio(
+    bucket_name = os.environ.get("S3_BUCKET")
+    minio = Minio(
         endpoint=os.environ.get("S3_HOSTNAME"),
         access_key=os.environ.get("S3_ACCESSKEY"),
         secret_key=os.environ.get("S3_SECRETKEY"),
